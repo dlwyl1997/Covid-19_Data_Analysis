5a343dbcdfdb58fa7bea33c797228016b1da96e4
Yulin WANG <905946576@qq.com>
2021-10-11 08:17:23 +0100

    Imported a Sample Website Template
    Imported a homepage website template.

--- covid19_data_analysis_platform/covid/views.py
+++ covid19_data_analysis_platform/covid/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    # return HttpResponse("Covid-19 Data Platform")
+    context_dict = {}
+    return render(request, 'covid/index.html', context=context_dict)
